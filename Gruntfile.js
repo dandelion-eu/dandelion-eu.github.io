3124065b66542783f4cd74a2651191a8377f034e
Martino Pizzol <martino.pizzol@gmail.com>
2013-05-13 14:16:39 +0200

    Fixed gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -147,7 +147,7 @@ module.exports = function (grunt) {
                 imagesDir: '<%= yeoman.app %>/images',
                 javascriptsDir: '<%= yeoman.app %>/scripts',
                 fontsDir: '<%= yeoman.app %>/styles/fonts',
-                importPath: 'app/components',
+//                importPath: 'app/components',
                 relativeAssets: true,
                 require: 'zurb-foundation'
             },
