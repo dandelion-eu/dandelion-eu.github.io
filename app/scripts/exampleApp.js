5e50ead4f217d8a8d887786abb11eb89d33de2c5
Martino Pizzol <martino.pizzol@gmail.com>
2013-05-15 10:28:45 +0200

    Added first version of logic to handle example in documentation page

---
+++ app/scripts/exampleApp.js
@@ -0,0 +1,50 @@
+(function(){
+    "use strict";
+    window.docsApp = {};
+
+    window.docsApp.Examples = Backbone.Collection.extend({
+    });
+
+    window.docsApp.DocView = Backbone.View.extend({
+        exampleTemplate: '<li><a title="<%= title %>" target="_blank" href="http://datamarket.spaziodati.eu/api/v1/<%=url%>"><%= prettyUrl %></a></li>',
+
+        initialize: function(){
+            _.bindAll(this, 'getElByTag');
+            this.exampleContainer = '.example-alert';
+            this.examples = this.$el.find(this.exampleContainer);
+            this.allTags = $.map(this.examples, function(el){
+                return $(el).data('example');
+            });
+            console.log(this.allTags);
+
+        },
+
+        getElByTag: function(tag){
+          return $(this.$el.find('[data-example='+tag+']'));
+        },
+
+        renderExample: function(example){
+          var context = {
+            url: encodeURI(example.get('url')),
+            prettyUrl: example.get('url'),
+            title: example.get('title', '')
+          };
+          return _.template(this.exampleTemplate, context);
+        },
+
+        render: function(){
+            var that = this;
+            _.each(this.allTags, function(tag){
+                var example = that.collection.where({featured: tag})
+                  , $tagEl;
+                if (example.length > 0){
+                    $tagEl = that.getElByTag(tag);
+                    $tagEl.removeClass('hidden');
+                    $tagEl.find('ul').append(that.renderExample(example[0]));
+                }
+            });
+            return this;
+        }
+    })
+
+})();
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,25 @@
 (function(){
     "use strict";
-    var apiSectionClass = '.api-section-title';
+    var apiSectionClass = '.api-section-title'
+      , exampleSelector = '.example-alert';
 
-    $(apiSectionClass).append('<span class="icon-link"></span>')
+    //TODO add a link
+    $(apiSectionClass).append('<span class="icon-link"></span>');
+    $(exampleSelector).addClass('hidden');
 
+    console.debug('reading example done');
     $.get('examples.yaml')
         .success(function (data){
-            console.log('reading example done');
-            window.doc = jsyaml.load(data);
+            var parsedData = window.doc = jsyaml.load(data);
+            console.debug('data readed ',parsedData);
+            window.docsApp.examples = new window.docsApp.Examples();
+            window.docsApp.examples.add(parsedData);
+
+            window.docsApp.docView = new window.docsApp.DocView(
+                {
+                    el: $('#doc-section'),
+                    collection: window.docsApp.examples
+                });
+            window.docsApp.docView.render();
         });
-    
 })();
