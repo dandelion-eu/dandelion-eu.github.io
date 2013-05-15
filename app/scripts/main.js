f5bd5dc0bcdd86cc8d6c45669c919b53365ee2a2
Martino Pizzol <martino.pizzol@gmail.com>
2013-05-15 08:28:05 +0200

    Added yaml reader

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,13 @@
 (function(){
+    "use strict";
     var apiSectionClass = '.api-section-title';
 
     $(apiSectionClass).append('<span class="icon-link"></span>')
+
+    $.get('examples.yaml')
+        .success(function (data){
+            console.log('reading example done');
+            window.doc = jsyaml.load(data);
+        });
+    
 })();
