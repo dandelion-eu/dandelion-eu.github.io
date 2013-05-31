4b65f279ff359545ba6b0a4e447cc8b0cd28b776
Stefano Parmesan <parmesan@spaziodati.eu>
2013-05-31 14:11:43 +0200

    add first version of the schema for the Places datagem

--- app/scripts/datagems.js
+++ app/scripts/datagems.js
@@ -1,7 +1,10 @@
 (function(){
 
     var Datagem = function(datagem, api){
-        this.fieldTemplate = _.template('<tr><td><%= field %></td><td><%= type %></td><td><%= doc %></td></td></tr>');
+        this.fieldTemplate = _.template('<tr><td><%= field %></td><td><%= type %></td><td><%= doc %></td><td></td><td><%= example %></td></tr>');
+        this.subfieldTemplate = _.template(
+          '<tr><td>↳ <%= field %></td><td><%= type %></td><td><%= doc %></td><td><a href="<%= links_to.uri %>"><%= links_to.label %></a></td><td><%= example %></td></tr>'
+        );
         this.datagem = datagem;
         this.api = api;
 
@@ -15,13 +18,36 @@
 
         $.get(yamlFile).success(function (yamlData) {
             var parsedData = jsyaml.load(yamlData) || {};
-            _.each(data, function(val, key){
-                $table.append(that.fieldTemplate({
-                    field: key,
-                    type: _.isObject(val) ? 'Composite' : val,
-                    doc: key in parsedData ? parsedData[key] : ''
-                }));
-            });
+            _.chain(Object.keys(data))
+                .sortBy(function(key){
+                  var idx = _.indexOf(Object.keys(parsedData), key);
+                  return (idx != -1) ? idx : 9999;
+                }).each(function(key) {
+                    var val = data[key];
+                    $table.append(that.fieldTemplate({
+                        field: key,
+                        type: _.isObject(val) ? 'Composite' : val,
+                        doc: key in parsedData ? parsedData[key]['description'] : '',
+                        example: key in parsedData ? parsedData[key]['example'] : ''
+                    }));
+
+                    if (_.isObject(val)) {
+                        var subfields = (key in parsedData && 'subfields' in parsedData[key]) ? parsedData[key].subfields : {};
+                        _.chain(Object.keys(val)).sortBy(function(sub_key){
+                            var idx = _.indexOf(Object.keys(subfields), sub_key);
+                            return (idx != -1) ? idx : 9999;
+                        }).each(function(sub_key){
+                            var sub_val = val[sub_key];
+                            $table.append(that.subfieldTemplate({
+                                field: key + "." + sub_key,
+                                type: sub_val,
+                                doc: sub_key in subfields ? subfields[sub_key]['description'] : '',
+                                links_to: sub_key in subfields && subfields[sub_key]['links_to'] ? subfields[sub_key]['links_to'] : '',
+                                example: sub_key in subfields ? subfields[sub_key]['example'] : ''
+                            }));
+                        });
+                    }
+                  });
         });
     };
 
